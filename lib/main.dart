7ab734fd79211e58d8e6ec5ce1969fd5475124a9
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-09-17 22:52:08 +0500

    added detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:book_store_app_ui/views/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Book Store',
       theme: ThemeData(
--- lib/views/book_detail_screen.dart
+++ lib/views/book_detail_screen.dart
@@ -1,6 +1,10 @@
 import 'package:book_store_app_ui/models/book_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating/flutter_rating.dart';
 import 'package:get/get.dart';
+
+bool isCart = false;
+bool isFavorite = false;
 
 class BookDetailScreen extends StatefulWidget {
   final Book book;
@@ -37,15 +41,172 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
             ),
             const SizedBox(width: 20),
             GestureDetector(
-              onTap: () {},
-              child: const Icon(
-                Icons.favorite,
-                color: Colors.red,
+              onTap: () {
+                setState(() {
+                  isFavorite = !isFavorite;
+                });
+              },
+              child: Icon(
+                isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: isFavorite ? Colors.red : Colors.black,
                 size: 25,
               ),
             ),
+            const SizedBox(width: 25),
           ],
         ),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.network(
+                      widget.book.cover,
+                      fit: BoxFit.fill,
+                      height: 260,
+                      width: MediaQuery.of(context).size.width / 2.5,
+                    ),
+                    const SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width / 2,
+                          child: Text(
+                            widget.book.title,
+                            style: const TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black54,
+                            ),
+                            softWrap: true,
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 25,
+                              backgroundImage: NetworkImage(
+                                widget.book.authorImg,
+                              ),
+                            ),
+                            const SizedBox(width: 15),
+                            Text(
+                              widget.book.author,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            StarRating(
+                              rating: widget.book.rating,
+                              size: 20,
+                              color: Colors.amber,
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              widget.book.rating.toString(),
+                              style: const TextStyle(
+                                fontSize: 15,
+                                color: Colors.black38,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Container(
+                  height: 110,
+                  padding:
+                      const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      moreMovieInfo(
+                        "https://img.freepik.com/premium-vector/comedy-icon-vector-image-can-be-used-movie-genres_120816-82986.jpg?w=740",
+                        widget.book.genre,
+                        "GENRE",
+                      ),
+                      const SizedBox(width: 5),
+                      moreMovieInfo(
+                        "https://cdn-icons-png.freepik.com/512/4951/4951447.png",
+                        widget.book.language,
+                        "LANGUAGE",
+                      ),
+                      const SizedBox(width: 5),
+                      moreMovieInfo(
+                        "https://cdn-icons-png.freepik.com/128/5824/5824120.png",
+                        widget.book.age,
+                        "AGE",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container moreMovieInfo(image, title, value) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(width: 2),
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 20,
+      ),
+      child: Row(
+        children: [
+          Image.network(
+            image,
+            height: 50,
+            width: 50,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(width: 15),
+          Column(
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  letterSpacing: 1.5,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                value,
+                style: const TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
